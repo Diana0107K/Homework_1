91f6a71da85113ebc0f5008167bbb211df1a9195
Diana0107K <97853647+Diana0107K@users.noreply.github.com>
2022-02-01 13:08:47 +0300

    Update Task_3.java

--- src/com/company/Task_3.java
+++ src/com/company/Task_3.java
@@ -2,15 +2,20 @@ package com.company;
 
 public class Task_3 {
     public static void main(String[] args) {
-        int dlinaMassiva3 = 10;
-        int[] Invent = new int[dlinaMassiva3];
-        for (int l = 0; l < Invent.length; l++) {
-            Invent[l] = (int) (Math.random() * 10);
-            System.out.print(Invent[l] + " ");
+        int arrayLength = 11;
+        int[] array3 = new int[arrayLength];
+        for (int l = 0; l < array3.length; l++) {
+            array3[l] = (int) (Math.random() * 10);
+            System.out.print(array3[l] + " ");
         }
         System.out.println();
-        for (int r = Invent.length - 1; r >= 0; r--) {
-            System.out.print(+Invent[r] + " ");
+        for (int r = 0; r < array3.length / 2; r ++) {
+            int tim = array3[r];
+            array3[r] = array3[array3.length - 1 - r];
+            array3[array3.length - 1 - r] = tim;
+        }
+        for (int r = 0; r < array3.length; r++) {
+            System.out.print(array3[r] + " ");
         }
     }
 }
